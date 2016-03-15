5ba2737087c6b64f0be2f72dc513526b5867ea72
Gonthalo <gonzalogalpmg@gmail.com>
2016-03-15 17:51:02 +0100

    Update object_detector.py

--- object_detector.py
+++ object_detector.py
@@ -6,24 +6,28 @@ import pylab as pl
 import numpy as np
 from random import random
 import cv2
+import sklearn
 from matplotlib import pyplot as plt
 
 matriz = []
 
-desviacion = [(0, 0), (0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, 1), (-1 , -1), (2, 1), (2, 2), (-2, 2), (-1, 2), (-2, -1)]
+desviacion = [(0, 0), (0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, 1), (-1 , -1), (2, 1), (2, 2), (-2, 2), (-1, 2), (-2, -1), (0, -2)]
+# esta es la forma del "punto" que la function generate() pone en la imagen
 
-def generate(n):
+def generate(n): #genera una imagen de prueba con "n" puntos de color aleatorio y fondo verde
 	img = Image.new("RGB", (400, 400), "black")
+	for ii in range(400):
+		for jj in range(400):
+			img.putpixel((ii, jj), (15 + int(random()*10), 220 + int(random()*10), 40 + int(random()*10)))
 	for ii in range(n):
 		px = 5 + int(random()*390)
 		py = 5 + int(random()*390)
 		color = tuple([int(random()*255) for kk in range(3)])
 		for xx, yy in desviacion:
-			if px + xx >=0 and px + xx < 400 and py + yy >=0 and py + yy < 400:
-				img.putpixel((px + xx, py + yy), color)
+			img.putpixel((px + xx, py + yy), color)
 	return img
 
-def get_shades(imag):
+def get_shades(imag): #toma una imagen PIL en color y devuelve tres imagenes en blanco y negro que coinciden con las capas de cada color
 	lista = []
 	for index in range(3):
 		imag2 = Image.new("L", imag.size, 0)
@@ -33,10 +37,10 @@ def get_shades(imag):
 		lista.append(imag2)
 	return tuple(lista)
 
-def parseimg(imag):
-	return [[imag.getpixel((ii, jj)) for jj in range(imag.size[0])] for ii in range(imag.size[1])];
+def parseimg(imag): #convierte una imagen PIL en una matriz
+	return [[imag.getpixel((ii, jj)) for jj in range(imag.size[0])] for ii in range(imag.size[1])]
 
-def get_points(archivo):
+def get_points(archivo): #identifica posibles puntos de interes en una imagen y los devuelve, mostrando su posicion en la imagen
 	total = []
 	imag = Image.open(archivo, "r")
 	for index in range(3):
@@ -50,7 +54,7 @@ def get_points(archivo):
 	imag = cv2.imread(archivo)
 	for i in np.array(total):
 	    x,y = i.ravel()
-	    cv2.circle(imag,(x,y),3,255,-1)
+	    cv2.circle(imag,(x,y),7,(245, 10, 10),1)
 	plt.imshow(imag),plt.show()
 	return total
 
@@ -60,9 +64,6 @@ imagen1.save("imagen_dron.png")
 
 print get_points("imagen_dron.png")
 
-print get_points("prado.jpg")
+#print get_points("prado.jpg")
 
-print get_points("monte.jpg")
-
-
-
+#print get_points("monte.jpg")
